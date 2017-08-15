a0c1e91e08d9577fd09afb44e7194d1018d843bf
Truman Purnell <truman.purnell@gmail.com>
2017-08-14 23:51:35 -0400

    UPDATE: adds text labels to each concept

--- src/browser/components/DataCircles.js
+++ src/browser/components/DataCircles.js
@@ -1,19 +1,44 @@
 import React from 'react';
 
 export default (graph) => {
+
+    // creates random sizes for concept circles, will allocate to most related topics below. See relational score.
+    let sizes
+    if (graph.children)
+        sizes = Array(graph.children.length)
+            .fill(null).map(rel => Math.floor(Math.random() * 150))
+            .sort((a, b) => a - b)
+
     return (
         <g>
             {
-                graph.children && graph.children.map((concept, index) => {
+                graph.children && graph.children.sort((a, b) => b.numOccur - a.numOccur).map((concept, index) => {
+                    const x = 2100 - Math.floor(Math.random() * 2000)
+                    const y = 2100 - Math.floor(Math.random() * 2000)
+                    const relationalScore = sizes.pop()
 
-                    const pos = {
-                        cx: 1200 - Math.floor(Math.random() * 1200),
-                        cy: 1200 - Math.floor(Math.random() * 1200),
-                        r: Math.floor(Math.random() * 200),
-                        key: index
-                    };
+                    const tAttrs = {
+                        fill: 'black',
+                        fontSize: '50px',
+                        stroke: 'black',
+                        x: x - 100,
+                        y
+                    }
 
-                    return <circle {...pos} />
+                    const cAttrs = {
+                        r: relationalScore,
+                        fill: 'pink',
+                        stroke: 'white',
+                        cx: x,
+                        cy: y
+                    }
+
+                    return (
+                        <g key={index}>
+                            <circle {...cAttrs} />
+                            <text {...tAttrs}>{concept.name}</text>
+                        </g>
+                    )
                 })
             }
         </g>
--- src/browser/components/ScatterPlot.js
+++ src/browser/components/ScatterPlot.js
@@ -5,10 +5,10 @@ import DataCircles from './DataCircles';
 
 export default ({ graph }) => {
 
-  const center = { cx: 1000, cy: 1000, r: 400 };
+  const center = { cx: 1000, cy: 1000, r: 200 };
 
   return (
-    <svg width={2000} height={2000}>
+    <svg width={8000} height={8000}>
       <circle {...center} />
       <DataCircles {...graph} />
     </svg>
