4432c167e2a06e0d15ef648035fb9d5acd67166c
Truman Purnell <truman.purnell@gmail.com>
2017-08-15 00:02:54 -0400

    CHORE: cleans up alignment in DataCircles.js

--- src/browser/components/DataCircles.js
+++ src/browser/components/DataCircles.js
@@ -10,37 +10,35 @@ export default (graph) => {
             .sort((a, b) => a - b)
 
     return (
-        <g>
-            {
-                graph.children && graph.children.sort((a, b) => b.numOccur - a.numOccur).map((concept, index) => {
-                    const x = 2100 - Math.floor(Math.random() * 2000)
-                    const y = 2100 - Math.floor(Math.random() * 2000)
-                    const relationalScore = sizes.pop()
+        <g>{
+            graph.children && graph.children.sort((a, b) => b.numOccur - a.numOccur).map((concept, index) => {
+                const x = 2100 - Math.floor(Math.random() * 2000)
+                const y = 2100 - Math.floor(Math.random() * 2000)
+                const relationalScore = sizes.pop()
 
-                    const tAttrs = {
-                        fill: 'black',
-                        fontSize: '50px',
-                        stroke: 'black',
-                        x: x - 100,
-                        y
-                    }
+                const tAttrs = {
+                    fill: 'black',
+                    fontSize: '50px',
+                    stroke: 'black',
+                    x: x - 100,
+                    y
+                }
 
-                    const cAttrs = {
-                        r: relationalScore,
-                        fill: 'pink',
-                        stroke: 'white',
-                        cx: x,
-                        cy: y
-                    }
+                const cAttrs = {
+                    r: relationalScore,
+                    fill: 'pink',
+                    stroke: 'white',
+                    cx: x,
+                    cy: y
+                }
 
-                    return (
-                        <g key={index}>
-                            <circle {...cAttrs} />
-                            <text {...tAttrs}>{concept.name}</text>
-                        </g>
-                    )
-                })
-            }
-        </g>
+                return (
+                    <g key={index}>
+                        <circle {...cAttrs} />
+                        <text {...tAttrs}>{concept.name}</text>
+                    </g>
+                )
+            })
+        }</g>
     )
 }
