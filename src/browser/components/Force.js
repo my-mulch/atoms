c98ea40cc398b7bec1dbc8c5f7f22122235f7ad4
Truman Purnell <truman.purnell@gmail.com>
2017-08-17 18:40:26 -0400

    CHORE: removes dead code from Force.js

--- src/browser/components/Force.js
+++ src/browser/components/Force.js
@@ -5,28 +5,23 @@ import { populate, init, update, simulate } from '../../d3/utils'
 import * as d3 from 'd3'
 
 
-let alive = false
-
 
 const Force = ({ graph, search }) => {
 
-    if (!Object.keys(graph).length) return null
+    if (Object.keys(graph).length) {
 
-    if (nodeGroup) {
-        nodeGroup.remove()
-        textGroup.remove()
-        linkGroup.remove()
+
+        const [width, height, svg,
+            linkGroup, nodeGroup, textGroup,
+            linkForce, forces, dragDrop] = init(d3)
+
+        const [nodes, links] = populate(graph)
+        const [linkElements, nodeElements, textElements] = update({ linkGroup, nodeGroup, textGroup, nodes, links, dragDrop })
+
+        simulate(forces, nodeElements, textElements, linkElements, nodes, links)
+
     }
 
-    const [width, height, svg,
-        linkGroup, nodeGroup, textGroup,
-        linkForce, forces, dragDrop] = init(d3)
-
-    const [nodes, links] = populate(graph)
-    const [linkElements, nodeElements, textElements] = update({ linkGroup, nodeGroup, textGroup, nodes, links, dragDrop })
-
-    simulate(forces, nodeElements, textElements, linkElements, nodes, links)
-    alive = true
     return null
 }
 
