f594a363646e19f9cecb99d6658d7779faa0cfc5
Anthony <awatson31911@gmail.com>
2017-08-17 20:25:14 -0400

    TEST: creates wikiScan tests

--- src/tests/react.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {expect} from 'chai';
-import {shallow} from 'enzyme';
-import {spy} from 'sinon';
-
-import Main from '../browser/compnents/Main';
-import Force from '../browser/compnents/Force';
-
-describe('React ')
