ef42448d0da24bf3bd9597e8559d7814fbec40c7
Kieran Derfus <kieranderfus@gmail.com>
2017-08-18 12:37:06 -0400

    UPDATE: AutoScroll on query submit

--- src/browser/components/Splash.js
+++ src/browser/components/Splash.js
@@ -2,45 +2,52 @@ import React from 'react'
 import { connect } from 'react-redux';
 import { search } from '../redux/concepts'
 import { getSuggestions } from '../redux/suggestions'
+import $ from 'jquery'
 
 import SecondSearch from './SecondSearch'
 import Force from './Force'
 
 const Splash = ({search, getSuggestions, suggestions}) => (
-    <div className="wrapper" id="wrapper-large">
-            <div className="container-fluid">
-                <h1 className="wow flipInY" data-wow-delay="1.5s">atomizer</h1>
-                <form
-                    autoComplete="off"
-                    className="form-inline search-form"
-                    onSubmit={evt => {
-                        evt.preventDefault();
-                        search(evt.target.query.value)
-                        document.getElementById('force').scrollIntoView(true)
-                    }}
-                    onChange={evt => {
-                        evt.preventDefault();
-                        getSuggestions(evt.target.value)
-                    }}
-                >
-                    <div className="input-group">
+    <div>
+        <section>
+            <div className="wrapper" id="wrapper-large">
+                <div className="container-fluid">
+                    <h1 className="wow flipInY" data-wow-delay="1.5s">atomizer</h1>
+                    <form
+                        autoComplete="off"
+                        className="form-inline search-form"
+                        onSubmit={evt => {
+                            evt.preventDefault();
+                            search(evt.target.query.value)
+                            $("html, body").animate({ scrollTop: 9999 }, 1000);
+                        }}
+                        onChange={evt => {
+                            evt.preventDefault();
+                            getSuggestions(evt.target.value)
+                        }}
+                    >
+                        <div className="input-group">
                     <span className="input-group-btn">
                         <input name="query" className="form-control" placeholder="Search"></input>
                     </span>
-                    </div>
-                    <ul className="dropdown">
-                        {
-                            suggestions && suggestions.map((suggestion, index) => {
-                                return <li key={index}>{suggestion}</li>
-                            })
-                        }
-                    </ul>
-                </form>
+                        </div>
+                        <ul className="dropdown">
+                            {
+                                suggestions && suggestions.map((suggestion, index) => {
+                                    return <li key={index}>{suggestion}</li>
+                                })
+                            }
+                        </ul>
+                    </form>
+                </div>
             </div>
-        <div id="force">
-            <Force />
-        </div>
-        </div>
+        </section>
+        <section>
+            <div id="force">
+                <Force/>
+            </div>
+        </section>
+    </div>
 )
 
 const mapState = ( { suggestions_reducer } ) => ({ suggestions: suggestions_reducer })
