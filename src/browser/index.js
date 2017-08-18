ebcbd2fb21ad469e6f79048a4487b8e361e2cc28
Kieran Derfus <kieranderfus@gmail.com>
2017-08-18 11:24:35 -0400

    CREATE: Splash Page

--- src/browser/index.js
+++ src/browser/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
 import Main from './components/Main'
+import SecondSearch from './components/SecondSearch'
 import store from './redux/store'
 
 ReactDOM.render(
   <Provider store={store}>
-    <Main />
+        <Main />
  </Provider>,
   document.getElementById("root")
 );
