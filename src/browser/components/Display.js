f8ea8706174462f29ea36b4b7a18252eae64fa64
Calvin <calvin.lin717@gmail.com>
2017-08-22 11:06:07 -0400

    switching to another branch

---
+++ src/browser/components/Display.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Query from './Query'
+
+const Display = () => (
+  <div id="display">
+    <h1> Testing </h1>
+  </div>
+)
+
+export default Display
--- src/browser/components/Main.js
+++ src/browser/components/Main.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Splash from './Splash'
 import Force from './Force'
 import Query from './Query'
+import Display from './Display'
 
 export default () => (
     <div className="wrapper" id="wrapper-large">
         <div className="container-fluid">
             <Splash />
+            <Display />
             <Force />
         </div>
     </div>
