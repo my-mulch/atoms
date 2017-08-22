0f8ee1a634c99ae4edf017be5ee2bd6ea1b33495
Anthony <awatson31911@gmail.com>
2017-08-21 23:57:59 -0400

    FEAT: submit query on li item click

--- src/browser/components/Query.js
+++ src/browser/components/Query.js
@@ -1,45 +1,72 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { search } from '../redux/graph'
-import { suggest, clearCompletions } from '../redux/completions'
+import { suggest, clearCompletions, select } from '../redux/completions'
 import $ from 'jquery'
 
-const Query = ({ search, suggest, completions, clearCompletions }) => (
-    <div id="query">
-        <form autoComplete="off" className="search-form"
-            onSubmit={(event) => handleSubmit(event, search, clearCompletions)}
-            onChange={(event) => handleChange(event, suggest, clearCompletions)}>
-            <div className="input-group">
-                <input name="query" className="form-control"></input>
-                <ul>{
-                    completions &&
-                    completions.map((suggestion, index) => (
-                        <li key={index}>{suggestion}</li>
-                    ))
-                }</ul>
-            </div>
+class Query extends Component {
+    constructor() {
+        super()
+        this.state = {
+            value: ''
+        }
 
-        </form>
-    </div>
-)
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleClick = this.handleClick.bind(this)
+    }
 
-const handleSubmit = (event, search, clearCompletions) => {
-    event.preventDefault();
-    search(event.target.query.value)
-    clearCompletions()
-    event.target.query.value = ''
+    render() {
+        const { completions, search, suggest, clearCompletions } = this.props
+
+        return (
+            <div id="query" >
+                <form autoComplete="off" className="search-form"
+                    onSubmit={(event) => this.handleSubmit(event, search, clearCompletions)}>
+                    <div className="input-group">
+                        <input
+                            name="query"
+                            className="form-control"
+                            value={this.state.value}
+                            onChange={(event) => this.handleChange(event, suggest, clearCompletions)}
+                        />
+                        <ul display={this.state.value !== '' ? "inline-block" : "none"}>{
+                            completions &&
+                            completions.map((suggestion, index) => (
+                                <li key={index} onClick={(event) => this.handleClick(suggestion, search)}>{suggestion}</li>
+                            ))}
+                        </ul>
+                    </div>
+
+                </form>
+            </div >
+        )
+    }
+
+    handleSubmit(event, search, clearCompletions) {
+        event.preventDefault()
+        search(this.state.value)
+        clearCompletions()
+        this.setState({ value: '' })
+    }
+
+    handleChange(event, suggest, clearCompletions) {
+        this.setState({ value: event.target.value })
+        this.state.value
+            ? suggest(this.state.value)
+            : clearCompletions()
+    }
+
+    handleClick(suggestion, search) {
+        search(suggestion)
+        this.setState({ value: '' })
+        clearCompletions()
+    }
 }
 
-const handleChange = (event, suggest, clearCompletions) => {
-    event.preventDefault();
-
-    event.target.value
-        ? suggest(event.target.value)
-        : clearCompletions()
-}
 
 const mapState = ({ completions }) => ({ completions })
-const mapDispatch = { search, suggest, clearCompletions }
+const mapDispatch = { search, suggest, select, clearCompletions }
 
 export default connect(mapState, mapDispatch)(Query);
 
