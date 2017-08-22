e3c7863eda1b0d2846c01b2baa52f89a0db6bfd5
Calvin <calvin.lin717@gmail.com>
2017-08-22 12:03:44 -0400

    CHORE: fixed background image consuming entire scroll screen

--- src/browser/components/Main.js
+++ src/browser/components/Main.js
@@ -2,13 +2,13 @@ import React from 'react'
 import Splash from './Splash'
 import Force from './Force'
 import Query from './Query'
-import Display from './Display'
 
 export default () => (
     <div className="wrapper" id="wrapper-large">
         <div className="container-fluid">
             <Splash />
-            <Display />
+        </div>
+        <div id="svg" className="container-fluid">
             <Force />
         </div>
     </div>
